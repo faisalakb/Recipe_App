48df6e7f57a1065ca6399ab6743a1fd6a5c6b6f1
anyars-encarta <anyarsencarta@gmail.com>
2023-12-05 12:35:07 +0000

    Create db and migrations

--- db/migrate/20231205085934_create_inventory_foods.rb
+++ db/migrate/20231205085934_create_inventory_foods.rb
@@ -1,11 +1,12 @@
 class CreateInventoryFoods < ActiveRecord::Migration[7.1]
-  def change
-    create_table :inventory_foods do |t|
-      t.integer :quantity
-      t.references :inventory, null: false, foreign_key: true
-      t.references :food, null: false, foreign_key: true
-
-      t.timestamps
+    def change
+      create_table :inventory_foods do |t|
+        t.integer :quantity
+        t.references :inventory, null: false, foreign_key: true
+        t.references :food, null: false, foreign_key: true
+  
+        t.timestamps
+      end
     end
   end
-end
+  

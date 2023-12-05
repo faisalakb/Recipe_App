0408c37b1364e5e2300ebf77ae92f1b73ad7de54
anyars-encarta <anyarsencarta@gmail.com>
2023-12-05 14:12:35 +0000

    Correct Rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  # A code goes here
 end
--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,24 +1,24 @@
 class FoodsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def index
-        @foods = current_user.foods
+  def index
+    @foods = current_user.foods
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = current_user.foods.new(food_params)
+    if @food.save
+      redirect_to foods_path, notice: 'Food was successfully created.'
+    else
+      render :new
     end
+  end
 
-    def new
-        @food = Food.new
-    end
-
-    def create
-        @food = current_user.foods.new(food_params)
-        if @food.save
-          redirect_to foods_path, notice: 'Food was successfully created.'
-        else
-          render :new
-        end
-    end
-
-    private
+  private
 
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)

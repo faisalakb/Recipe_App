7e4c720aefc2476e9091a4b547b9df0b125075b8
anyars-encarta <anyarsencarta@gmail.com>
2023-12-05 16:29:21 +0000

    Added and configured devise for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true
 
-  has_many :foods, foreign_key: 'author_id'
+  has_many :foods, foreign_key: 'user_id'
   has_many :recipes, dependent: :destroy
 end

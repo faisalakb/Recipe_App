0752bd0c388dc6fca0a36cdf459d6b1122c92a1d
anyars-encarta <anyarsencarta@gmail.com>
2023-12-05 15:39:11 +0000

    Install devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
+# user_controller.rb
 class UsersController < ApplicationController
-end
+    before_action :set_user, only: [:show, :destroy]
+    before_action :authenticate_user!
+    
+    def login
+      @user = User.new
+    end
+  
+    def index
+      @users = User.all
+    end
+  
+    def destroy
+      @user.destroy
+      redirect_to users_path
+    end
+  
+    def show
+    end
+  
+    private
+  
+    def set_user
+      @user = User.find_by_id(params[:id])
+    end
+  end
+  

951f22bb7c9ed77b2e122245060db59c5d92f0df
anyars-encarta <anyarsencarta@gmail.com>
2023-12-09 00:20:26 +0000

    Correct CSS

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,10 +1,8 @@
 class Food < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
+
   has_many :inventory_foods, dependent: :destroy
   validates :user_id, presence: true
-=======
-  has_many :inventory_foods
+
   has_many :inventories, through: :inventory_foods
->>>>>>> dev
 end
--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -1,10 +1,6 @@
 # app/models/inventory_food.rb
 class InventoryFood < ApplicationRecord
-<<<<<<< HEAD
+
   belongs_to :inventory, foreign_key: 'inventory_id'
   belongs_to :food, foreign_key: 'food_id'
-=======
-  belongs_to :inventory
-  belongs_to :food
->>>>>>> dev
 end

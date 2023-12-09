15475e88a0b88c1a98b65d88e9d27a668682599a
anyars-encarta <anyarsencarta@gmail.com>
2023-12-09 09:51:23 +0000

    Remove git info from application.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -26,17 +26,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_08_152843) do
 
   create_table "inventories", force: :cascade do |t|
     t.string "name"
-    t.string "description"
-    t.bigint "users_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["users_id"], name: "index_inventories_on_users_id"
+    t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
   create_table "inventory_foods", force: :cascade do |t|
     t.integer "quantity"
-    t.bigint "inventory_id"
-    t.bigint "food_id"
+    t.bigint "inventory_id", null: false
+    t.bigint "food_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["food_id"], name: "index_inventory_foods_on_food_id"
@@ -78,12 +77,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_08_152843) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "password_digest"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "inventories", "users", column: "users_id"
+  add_foreign_key "inventories", "users"
   add_foreign_key "inventory_foods", "foods"
   add_foreign_key "inventory_foods", "inventories"
   add_foreign_key "recipes", "inventories"
